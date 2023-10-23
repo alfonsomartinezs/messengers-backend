01992dd0cc84578c2ac43eeff653d8af4a4cf665
alphaandsew <alphaandsew@gmail.com>
2023-10-23 08:30:30 -0700

    rubocop formatting

--- db/migrate/20231021050206_add_fractal_relationships.rb
+++ db/migrate/20231021050206_add_fractal_relationships.rb
@@ -1,5 +1,5 @@
 class AddFractalRelationships < ActiveRecord::Migration[7.0]
   def change
-    add_reference(:fractals, :parent, foreign_key: {to_table: :fractals})
+    add_reference(:fractals, :parent, foreign_key: { to_table: :fractals })
   end
 end
